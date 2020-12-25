a8a533720c13c973b333a64a2c55c77f9b4e29bd
mpampolina <miguel.r.pampolina@gmail.com>
2020-12-25 13:57:08 -0800

    add footer section and add-link-section web component

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
  components themselves.
  - If <slot>'s are not used, child HTML elements will not
  nest properly within their parent element
+ - A single <slot> element in the parent web component
+ template can house multiple child elements (as long as
+ they all share the same "slot" attribute) 
  - When dealing with nested web components through slotting,
  the parent web component's styles do not inherit downwards
  (as should be the case with web components). However, if
@@ -55,6 +58,7 @@ boardTemplate.innerHTML = `
       background-color: white;
       border: none;
       border-radius: 15px;
+      cursor: pointer;
     }
     
     .board-related-actions:focus {
@@ -72,11 +76,11 @@ boardTemplate.innerHTML = `
       </button>
     </div>
     <slot name="link-line"></slot>
+    <slot name="add-link-section"></slot>
   </div>
-`
+`;
 
-const linkTemplate = document.createElement("template")
-
+const linkTemplate = document.createElement("template");
 linkTemplate.innerHTML = `
   <style>
     * {
@@ -112,6 +116,7 @@ linkTemplate.innerHTML = `
       font-weight: 300;
       background-color: none;
       border: none;
+      cursor: pointer;
     }
     
     .link-line-actions--control a {
@@ -133,14 +138,130 @@ linkTemplate.innerHTML = `
       <button>delete</button>
     </div>
   </div>
-`
+`;
+
+const addLinkSectionTemplate = document.createElement("template");
+addLinkSectionTemplate.innerHTML = `
+  <style>
+    /* Add Link Button Styling */
+
+    * {
+      margin: 0px 0px;
+      padding: 0px 0px;
+      box-sizing: border-box
+    }
+
+    .add-link-form {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .add-link-btn--container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    
+    .add-link-btn {
+      margin: 10px auto;
+      padding: 10px 20px 5px 20px;
+      font-family: "Nunito", sans-serif;
+      font-weight: 300;
+      font-size: 1rem;
+      background-color: white;
+      color: var(--dark-grey);
+      border: none;
+      border-bottom: 2px solid white;
+      cursor: pointer;
+    }
+
+    .add-link-btn:hover {
+      border-bottom: 2px solid var(--light-blue);
+    }
+
+    .add-link-btn:focus {
+      outline: none;
+    }
+    
+    .add-link-btn--container .hide-elem {
+      display: none;
+    }
+
+    /* Form Styling */
+
+    .form {
+      margin: 10px 10px;
+      padding: 10px 20px;
+      background-color: var(--grey-white);
+      color: var(--dark-grey);
+      border-radius: 10px;
+    }
+    
+    .input {
+      font-family: "Nunito", sans-serif;
+      font-size: 1.2rem;
+      background-color: var(--grey-white);
+      color: var(--dark-grey);
+      border: none;
+      border-bottom: 2px solid var(--dark-grey);
+      margin: 10px 0px;
+    }
+    
+    .input:focus {
+      outline: none;
+    }
+    
+    .btn-control {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+    }
+    
+    .form-btn {
+      padding: 10px 5px 0px 5px;
+      margin: 5px 5px;
+      font-family: "Nunito", sans-serif;
+      font-weight: 300;
+      font-size: 1.5rem;
+      background-color: none;
+      color: var(--dark-grey);
+      border: none;
+      border-bottom: 2px solid var(--grey-white);
+      cursor: pointer;
+    }
+    
+    .form-btn:hover {
+      border-bottom: 2px solid var(--light-blue)
+    }
+    
+    .form-btn:focus {
+      outline: none;
+    }
+  </style>
+
+  <div class="add-link-btn--container">
+    <button class="add-link-btn">
+      <h4>-- Add New Link --</h4>
+    </button>
+    <form action="" class="add-link-form hide-elem form">
+      <label for="link-url-input">Link URL</label>
+      <input class="input" type="text" name="link-url-input" placeholder="http:// ...">
+      <label for="link-title-input">Link Title</label>
+      <input class="input" type="text" name="link-title-input" placeholder="insert awesome website here">
+      <div class="btn-control">
+        <button class="form-btn" type="submit">save</button>
+        <button class="form-btn cancel-btn" type="button">cancel</button>
+      </div>
+    </form>
+  </div>
+`;
 
 class LinkBoard extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(boardTemplate.content.cloneNode(true))
-    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("title")
+    this.shadowRoot.appendChild(boardTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("title");
   }
 }
 
@@ -148,43 +269,48 @@ class LinkLine extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(linkTemplate.content.cloneNode(true))
-    this.shadowRoot.querySelector("h4").innerText = this.getAttribute("linkTitle")
-    this.shadowRoot.querySelector("p").innerText = this.getAttribute("linkUrl")
-    this.shadowRoot.querySelector("a").href = this.getAttribute("linkUrl")
+    this.shadowRoot.appendChild(linkTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector("h4").innerText = this.getAttribute(
+      "linkTitle"
+    );
+    this.shadowRoot.querySelector("p").innerText = this.getAttribute("linkUrl");
+    this.shadowRoot.querySelector("a").href = this.getAttribute("linkUrl");
+  }
+}
+
+class AddLinkSection extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    const sr = this.shadowRoot;
+    sr.appendChild(addLinkSectionTemplate.content.cloneNode(true));
+
+    /* Enable opening and closing the add-link-form */
+    const toggleLinkFormArr = [
+      sr.querySelector(".add-link-btn"),
+      sr.querySelector(".cancel-btn"),
+    ];
+    const addLinkForm = sr.querySelector(".add-link-form");
+    toggleLinkFormArr.forEach((element) => {
+      element.addEventListener("click", () => {
+        addLinkForm.classList.toggle("hide-elem");
+      });
+    });
   }
 }
 
 window.customElements.define("link-board", LinkBoard);
 window.customElements.define("link-line", LinkLine);
+window.customElements.define("add-link-section", AddLinkSection);
 
-const addBoardBtn = document.querySelector("#add-board-btn")
-const addBoardForm = document.querySelector('#add-board-btn--form')
-const addLinkBtn = document.querySelector(".add-link-btn")
-const addLinkForm = document.querySelector(".add-link-form")
-const addBoardBtnCancel = document.querySelector("#add-board-btn--cancel")
-const cancelBtn = document.querySelector(".cancel-btn")
+const addBoardBtnForm = document.querySelector("#add-board-btn--form");
+const toggleBoardFormArr = [
+  document.querySelector("#add-board-btn"),
+  document.querySelector("#add-board-btn--cancel"),
+];
 
-function show() {
-  addLinkForm.classList.toggle('hide-elem');
-}
-
-function show2() {
-  addBoardForm.classList.toggle('hide-elem')
-}
-
-addLinkBtn.addEventListener("click", (e) => {
-  show();
-})
-
-cancelBtn.addEventListener("click", (e) => {
-  show();
-})
-
-addBoardBtn.addEventListener("click", (e) => {
-  show2();
-})
-
-addBoardBtnCancel.addEventListener("click", (e) => {
-  show2();
-})
+toggleBoardFormArr.forEach((element) => {
+  element.addEventListener("click", () => {
+    addBoardBtnForm.classList.toggle("hide-elem");
+  });
+});
