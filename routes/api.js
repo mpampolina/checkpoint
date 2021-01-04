64e1dcf4be601299524b6cdd3e7366d097ee312c
mpampolina <miguel.r.pampolina@gmail.com>
2021-01-03 21:20:37 -0800

    add database calls to api endpoints

--- routes/api.js
+++ routes/api.js
@@ -6,44 +6,86 @@ const User = require("../models/User.js");
 const { Board, boardSchema } = require("../models/Board.js");
 const { Link, linkSchema } = require("../models/Link.js");
 
-/* GET request to retrieve all boards */
-
 /* POST request to create new board */
 router.post("/", (req, res) => {
   const { board_title } = req.body;
-  console.log(`COMMAND - post - USER - ${req.user.name}`);
+  const new_board = new Board({ board_title: board_title, links: [] });
 
-  res.status("200").json({ success: true, _id: ObjectId() });
+  User.updateOne(
+    { _id: req.user._id },
+    {
+      $push: { boards: new_board },
+    }
+  )
+    .then((db_res) => {
+      console.log(db_res);
+      res.status(200).json({ success: true, _id: new_board._id });
+    })
+    .catch((err) => {
+      res.status(200).json({ success: false });
+      console.log(err);
+    });
 });
 
 /* DELETE request to delete a current board */
 router.delete("/:boardId", (req, res) => {
   const board_id = req.params.boardId;
-  console.log(`COMMAND - delete - USER - ${req.user.name}`);
-  console.log(`board_id: ${board_id}`);
 
-  res.status(200).json({ success: true });
+  User.updateOne(
+    { _id: req.user._id },
+    { $pull: { boards: { _id: ObjectId(board_id) } } }
+  )
+    .then((db_res) => {
+      console.log(db_res);
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(200).json({ success: false });
+      console.log(err);
+    });
 });
 
 /* PUT request to add a new link */
 router.put("/add", (req, res) => {
   const { link_title, url, board_id } = req.body;
-  console.log(`COMMAND - put/add - USER - ${req.user.name}`);
-  console.log(`link_title: ${link_title}`);
-  console.log(`url: ${url}`);
-  console.log(`board_id: ${board_id}`);
+  const new_link = new Link({ link_title: link_title, url: url });
 
-  res.status(200).json({ success: true, _id: ObjectId() });
+  User.updateOne(
+    { _id: req.user._id, "boards._id": ObjectId(board_id) },
+    { $push: { "boards.$.links": new_link } }
+  )
+    .then((res_db) => {
+      console.log(res_db);
+      res.status(200).json({ success: true, _id: new_link._id });
+    })
+    .catch((err) => {
+      res.status(200).json({ success: false });
+      console.log(err);
+    });
 });
 
 /* PUT request to delete a current link */
 router.put("/delete", (req, res) => {
   const { link_id, board_id } = req.body;
-  console.log(`COMMAND - put/delete - USER - ${req.user.name}`);
-  console.log(`link_id: ${link_id}`);
-  console.log(`board_id: ${board_id}`);
-
-  res.status(200).json({ success: true });
+  User.updateOne(
+    {
+      _id: req.user._id,
+      "boards._id": ObjectId(board_id),
+    },
+    {
+      $pull: {
+        "boards.$.links": { _id: ObjectId(link_id) },
+      },
+    }
+  )
+    .then((res_db) => {
+      console.log(res_db);
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(200).json({ success: false });
+      console.log(err);
+    });
 });
 
 module.exports = router;
